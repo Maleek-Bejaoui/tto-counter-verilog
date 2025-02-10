583c9fc79e751b58bd180f540e7a3f22625d51bf
Maleek <101218361+Maleek-Bejaoui@users.noreply.github.com>
2025-02-10 01:17:37 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -15,11 +15,11 @@ module tb ();
 
   // Wire up the inputs and outputs:
   reg clk;
-  reg rst;
-  //reg ena;
+  reg rst_n;
+  reg ena;
   reg [7:0] ui_in;
   reg [7:0] uio_in;
-   wire [7:0] cmpt;
+   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 `ifdef GL_TEST
@@ -37,13 +37,13 @@ module tb ();
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
-      .cmpt (uo_out),   // Dedicated outputs
+      .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst  (rst)     // not reset
+      .rst_n  (rst)     // not reset
   );
 
 endmodule
